53e036700a2fb0909b2148829ab82513dc152d2e
yaroslav <ydpolo@mail.ru>
2025-06-23 10:51:15 +0300

    fix getting full image url

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 const getFullUrl = (req, relativePath) => {
-  return `${process.env.PROTOCOL || 'http'}://${req.get('host')}/${relativePath}`
+  return `${process.env.PROTOCOL || 'http'}://${process.env.URL || ('127.0.0.1' + (process.env.PORT || '8000'))}/${relativePath}`
 }
 
 export { getFullUrl }
