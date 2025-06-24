28b78f5622748735e1a70a0b75733980e94f4247
yaroslav <ydpolo@mail.ru>
2025-06-24 18:19:30 +0300

    fix api/companyManagers/order/all

--- src/controllers/companyManagerController.js
+++ src/controllers/companyManagerController.js
@@ -624,7 +624,7 @@ export const getAll = async (req, res) => {
           continue
         }
 
-        if (order.driver.hasOwnProperty('contragent_id') && order.driver.contragent_id && order.driver.contragent_id === contragentId) {
+        if (order.hasOwnProperty('driver') && order.driver && order.driver.hasOwnProperty('contragent_id') && order.driver.contragent_id && order.driver.contragent_id === contragentId) {
           orders.push(order)
         }
       }
